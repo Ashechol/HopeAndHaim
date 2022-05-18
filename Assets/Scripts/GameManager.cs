824bf7c5e4962f160b40a9a2a86038d33f1824ec
ashechol <ashechol@qq.com>
2022-05-18 16:28:50 +0800

    fix merge conflict

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,56 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// 管理游戏进程
 /// </summary>
 public class GameManager : Singleton<GameManager>
 {
-<<<<<<< HEAD
-    List<IGameObserver> _observers;
-
-=======
     public enum GameMode
     {
         Normal,         //操控角色
         GamePlay,       //播放动画
         DialogueMoment  //对话暂停
     }
+    List<IGameObserver> _observers;
+    Scene _currentScene;
     public GameMode gameMode;
 
-    #region 组件
     public Hope hope;
     // Haim one of the main actors
->>>>>>> 190969af5d2a13b40b826c7061754fe878a77289
     public Haim haim;
-    #endregion
 
-    private void Start()
+    void Start()
     {
-        //根据 Director 状态设置 GameMode
-        if (TimelineManager.Instance.currentDirector.playOnAwake) {
-            gameMode = GameMode.GamePlay;
-        }
-        else {
-            gameMode = GameMode.Normal;
-        }
+        _currentScene = SceneManager.GetActiveScene();
+
+        if (_currentScene.name == "Episode One")
+            EpisodeOneStart();
     }
 
-    private void Update()
+    void Update()
     {
-        //对话暂停时，需要按键恢复
-        if (gameMode == GameMode.DialogueMoment) {
-            if (Input.GetKeyDown(KeyCode.E)) {
-                TimelineManager.Instance.ResumeTimeline();
-            }
-        }
-        //加速播放，调试用
-        else if (gameMode == GameMode.GamePlay) {
-            if (Input.GetKeyDown(KeyCode.Q)) {
-                TimelineManager.Instance.SpeedUp();
-            }
-        }
+        if (_currentScene.name == "Episode One")
+            EpisodeOneUpdate();
     }
 
     public Vector3 PlayerPosition { get { return haim.transform.position; } }
@@ -61,7 +44,6 @@ public class GameManager : Singleton<GameManager>
         this.haim = haim;
     }
 
-<<<<<<< HEAD
     public void AddObserver(IGameObserver observer)
     {
         _observers.Add(observer);
@@ -71,7 +53,40 @@ public class GameManager : Singleton<GameManager>
     {
         _observers.Remove(observer);
     }
-=======
+
+    void EpisodeOneStart()
+    {
+        //根据 Director 状态设置 GameMode
+        if (TimelineManager.Instance.currentDirector.playOnAwake)
+        {
+            gameMode = GameMode.GamePlay;
+        }
+        else
+        {
+            gameMode = GameMode.Normal;
+        }
+    }
+
+    void EpisodeOneUpdate()
+    {
+        //对话暂停时，需要按键恢复
+        if (gameMode == GameMode.DialogueMoment)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                TimelineManager.Instance.ResumeTimeline();
+            }
+        }
+        //加速播放，调试用
+        else if (gameMode == GameMode.GamePlay)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                TimelineManager.Instance.SpeedUp();
+            }
+        }
+    }
+
     #region GameMode 函数
     /// <summary>
     /// 判断当前的游戏模式是否接收用户输入
@@ -81,5 +96,4 @@ public class GameManager : Singleton<GameManager>
         return gameMode == GameMode.Normal;
     }
     #endregion
->>>>>>> 190969af5d2a13b40b826c7061754fe878a77289
 }
